7c2efb7a3cf8bea5fc81c1aada2fac41156f9e77
Andrew Or <andrewor@fb.com>
2022-05-11 16:13:40 -0700

    [Quant][fx][bc-breaking] Replace qconfig_dict with a config object
    Summary: Previously, FX graph mode quantization configurations
    were specified through a dictionary of qconfigs. However, this
    API was not in line with other core APIs in PyTorch. This commit
    replaces this dictionary with a config object that users will
    create and pass to prepare and convert. This leads to better
    type safety and better user experience in notebook settings
    due to improved auto completion.
    
    The new API is as follows (name of config object is still
    subject to change):
    
    ```
    from torch.ao.quantization.quantization_config import PrepareQuantizationConfig
    from torch.ao.quantization.quantize_fx import prepare_fx
    
    quantization_config = PrepareQuantizationConfig()
        .set_global(qconfig)
        .set_object_type(torch.nn.Linear, qconfig)
        .set_module_name_regex("foo.*bar", qconfig)
        .set_module_name("mod", qconfig)
    
    prepare_fx(model, quantization_config)
    ```
    
    For backwards compatibility, `prepare_fx`, `prepare_qat_fx`,
    and `convert_fx` will continue to accept qconfig_dicts, which
    will be converted to QConfigContainers internally.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers: jerryzh168, vkuzo, HDCharles
    
    Subscribers: jerryzh168, vkuzo, HDCharles
    
    ghstack-source-id: b21119fa9a5eb3b3af4bca83cf779484fecbaeba
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/77147

---
+++ torch/ao/quantization/quantization_config.py
@@ -0,0 +1,171 @@
+from __future__ import annotations
+from collections import OrderedDict
+from dataclasses import astuple, dataclass
+from typing import Any, Callable, Dict, List, Type, TypeVar, Union
+
+from .qconfig import QConfigAny
+
+
+__all__ = [
+    "ConvertQuantizationConfig",
+    "EqualizationConfig",
+    "PrepareQuantizationConfig",
+    "QConfigModuleNameEntry",
+    "QConfigModuleNameRegexEntry",
+    "QConfigModuleNameObjectTypeOrderEntry",
+    "QConfigObjectTypeEntry",
+    "QuantizationConfigBase",
+]
+
+
+# TODO: replace all usages with these constants
+GLOBAL_DICT_KEY = ""
+OBJECT_TYPE_DICT_KEY = "object_type"
+MODULE_NAME_REGEX_DICT_KEY = "module_name_regex"
+MODULE_NAME_DICT_KEY = "module_name"
+MODULE_NAME_OBJECT_TYPE_ORDER_DICT_KEY = "module_name_object_type_order"
+
+
+@dataclass
+class QConfigObjectTypeEntry:
+    # object_type can be
+    # 1. module type (call_module)
+    # 2. function (call_function)
+    # 3. string (call_method)
+    object_type: Union[Callable, str]
+    qconfig: QConfigAny
+
+
+@dataclass
+class QConfigModuleNameRegexEntry:
+    module_name_regex: str
+    qconfig: QConfigAny
+
+
+@dataclass
+class QConfigModuleNameEntry:
+    module_name: str
+    qconfig: QConfigAny
+
+
+@dataclass
+class QConfigModuleNameObjectTypeOrderEntry:
+    module_name: str
+    object_type: Callable
+    index: int
+    qconfig: QConfigAny
+
+
+_T = TypeVar("_T", bound="QuantizationConfigBase")
+
+class QuantizationConfigBase:
+    """
+    TODO: write this
+    """
+
+    def __init__(self):
+        # In increasing match priority:
+        self.global_qconfig: QConfigAny = None
+        self.object_type_qconfigs: List[QConfigObjectTypeEntry] = []
+        self.module_name_regex_qconfigs: List[QConfigModuleNameRegexEntry] = []
+        self.module_name_qconfigs: List[QConfigModuleNameEntry] = []
+        self.module_name_object_type_order_qconfigs: List[QConfigModuleNameObjectTypeOrderEntry] = []
+
+        # For mypy warnings
+        self._object_type_qconfig_dict: OrderedDict[Union[Callable, str], QConfigAny] = OrderedDict()
+        self._module_name_regex_qconfig_dict: OrderedDict[str, QConfigAny] = OrderedDict()
+        self._module_name_qconfig_dict: OrderedDict[str, QConfigAny] = OrderedDict()
+
+    def set_global(self: _T, global_qconfig: QConfigAny) -> _T:
+        """
+        TODO: write this
+        """
+        self.global_qconfig = global_qconfig
+        return self
+
+    def set_object_type(self: _T, object_type: Callable, qconfig: QConfigAny) -> _T:
+        """
+        TODO: write this
+        """
+        self.object_type_qconfigs.append(QConfigObjectTypeEntry(object_type, qconfig))
+        return self
+
+    def set_module_name_regex(self: _T, module_name_regex: str, qconfig: QConfigAny) -> _T:
+        """
+        TODO: write this
+        """
+        self.module_name_regex_qconfigs.append(QConfigModuleNameRegexEntry(module_name_regex, qconfig))
+        return self
+
+    def set_module_name(self: _T, module_name: str, qconfig: QConfigAny) -> _T:
+        """
+        TODO: write this
+        """
+        self.module_name_qconfigs.append(QConfigModuleNameEntry(module_name, qconfig))
+        return self
+
+    def set_module_name_object_type_order(
+            self: _T,
+            module_name: str,
+            object_type: Callable,
+            index: int,
+            qconfig: QConfigAny) -> _T:
+        """
+        TODO: write this
+        """
+        self.module_name_object_type_order_qconfigs.append(
+            QConfigModuleNameObjectTypeOrderEntry(module_name, object_type, index, qconfig))
+        return self
+
+    def to_dict(self) -> Dict[str, Any]:
+        """
+        TODO: write this
+        """
+        def to_tuple_list(l):
+            return [astuple(e) for e in l]
+        return {
+            GLOBAL_DICT_KEY: self.global_qconfig,
+            OBJECT_TYPE_DICT_KEY: to_tuple_list(self.object_type_qconfigs),
+            MODULE_NAME_REGEX_DICT_KEY: to_tuple_list(self.module_name_regex_qconfigs),
+            MODULE_NAME_DICT_KEY: to_tuple_list(self.module_name_qconfigs),
+            MODULE_NAME_OBJECT_TYPE_ORDER_DICT_KEY: to_tuple_list(self.module_name_object_type_order_qconfigs),
+        }
+
+    @classmethod
+    def from_dict(cls: Type[_T], qconfig_dict: Dict[str, Any]) -> _T:
+        """
+        TODO: write this
+        """
+        conf = cls()
+        if GLOBAL_DICT_KEY in qconfig_dict:
+            conf.set_global(qconfig_dict[GLOBAL_DICT_KEY])
+        for object_type, qconfig in qconfig_dict.get(OBJECT_TYPE_DICT_KEY, []):
+            conf.set_object_type(object_type, qconfig)
+        for module_name_regex, qconfig in qconfig_dict.get(MODULE_NAME_REGEX_DICT_KEY, []):
+            conf.set_module_name_regex(module_name_regex, qconfig)
+        for module_name, qconfig in qconfig_dict.get(MODULE_NAME_DICT_KEY, []):
+            conf.set_module_name(module_name, qconfig)
+        for module_name, object_type, index, qconfig in qconfig_dict.get(MODULE_NAME_OBJECT_TYPE_ORDER_DICT_KEY, []):
+            conf.set_module_name_object_type_order(module_name, object_type, index, qconfig)
+        return conf
+
+
+class EqualizationConfig(QuantizationConfigBase):
+    """
+    TODO: write this
+    """
+    pass
+
+
+class PrepareQuantizationConfig(QuantizationConfigBase):
+    """
+    TODO: write this
+    """
+    pass
+
+
+class ConvertQuantizationConfig(QuantizationConfigBase):
+    """
+    TODO: write this
+    """
+    pass
--- torch/ao/quantization/quantize_fx.py
+++ torch/ao/quantization/quantize_fx.py
@@ -1,4 +1,6 @@
-from typing import Dict, Any, List, Callable, Tuple, Optional, Set
+import copy
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
+import warnings
 
 import torch
 from torch.fx import GraphModule
@@ -14,10 +16,10 @@ from .fx.qconfig_utils import (
     check_is_valid_convert_custom_config_dict,
     check_is_valid_fuse_custom_config_dict,
     check_is_valid_prepare_custom_config_dict,
-    check_is_valid_qconfig_dict,
 )
 from .fx.utils import graph_pretty_str  # noqa: F401
 from .fx.utils import get_custom_module_class_keys  # noqa: F401
+from .quantization_config import ConvertQuantizationConfig, EqualizationConfig, PrepareQuantizationConfig
 
 
 def _check_is_graph_module(model: torch.nn.Module) -> None:
@@ -176,16 +178,16 @@ class QuantizationTracer(Tracer):
 
 def _prepare_fx(
     model: torch.nn.Module,
-    qconfig_dict: Any,
+    quantization_config: Union[PrepareQuantizationConfig, Dict[str, Any]],
     is_qat: bool,
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
-    equalization_qconfig_dict: Optional[Dict[str, Any]] = None,
+    equalization_config: Optional[Union[EqualizationConfig, Dict[str, Any]]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
     is_standalone_module: bool = False,
 ) -> ObservedGraphModule:
     r""" Internal helper function for prepare_fx
     Args:
-      `model`, `qconfig_dict`, `prepare_custom_config_dict`, `equalization_qonfig_dict`:
+      `model`, `quantization_config`, `prepare_custom_config_dict`, `equalization_config`:
       see docs for :func:`~torch.ao.quantization.prepare_fx`
       `is_standalone_module`: a boolean flag indicates whether we are
       quantizing a standalone module or not, a standalone module
@@ -196,12 +198,27 @@ forward graph of the parent module,
     """
     if prepare_custom_config_dict is None:
         prepare_custom_config_dict = {}
-    if equalization_qconfig_dict is None:
-        equalization_qconfig_dict = {}
+    if equalization_config is None:
+        equalization_config = EqualizationConfig()
 
-    check_is_valid_qconfig_dict(qconfig_dict)
     check_is_valid_prepare_custom_config_dict(prepare_custom_config_dict)
-    check_is_valid_qconfig_dict(equalization_qconfig_dict)
+
+    if isinstance(quantization_config, Dict):
+        warnings.warn(
+            "Passing a QConfig dictionary to prepare is deprecated and will not be supported "
+            "in a future version. Please pass in a PrepareQuantizationConfig instead.")
+        quantization_config = PrepareQuantizationConfig.from_dict(quantization_config)
+
+    if isinstance(equalization_config, Dict):
+        warnings.warn(
+            "Passing a QConfig dictionary to prepare for equalization is deprecated and will not "
+            "be supported in a future version. Please pass in a EqualizationConfig instead.")
+        equalization_config = EqualizationConfig.from_dict(equalization_config)
+
+    assert(isinstance(quantization_config, PrepareQuantizationConfig))
+    assert(isinstance(equalization_config, EqualizationConfig))
+    quantization_config = copy.deepcopy(quantization_config)
+    equalization_config = copy.deepcopy(equalization_config)
 
     skipped_module_names = prepare_custom_config_dict.get(
         "non_traceable_module_name", []
@@ -244,11 +261,11 @@ forward graph of the parent module,
         backend_config_dict)
     prepared = prepare(
         graph_module,
-        qconfig_dict,
+        quantization_config,
         is_qat,
         tracer.node_name_to_scope,
         prepare_custom_config_dict=prepare_custom_config_dict,
-        equalization_qconfig_dict=equalization_qconfig_dict,
+        equalization_config=equalization_config,
         backend_config_dict=backend_config_dict,
         is_standalone_module=is_standalone_module,
     )  # type: ignore[operator]
@@ -260,7 +277,7 @@ forward graph of the parent module,
 
 def _prepare_standalone_module_fx(
     model: torch.nn.Module,
-    qconfig_dict: Any,
+    quantization_config: Union[PrepareQuantizationConfig, Dict[str, Any]],
     is_qat: bool,
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
@@ -289,7 +306,7 @@ def _prepare_standalone_module_fx(
     """
     return _prepare_fx(
         model,
-        qconfig_dict,
+        quantization_config,
         is_qat,
         prepare_custom_config_dict,
         backend_config_dict=backend_config_dict,
@@ -339,11 +356,12 @@ def fuse_fx(
 
 def prepare_fx(
     model: torch.nn.Module,
-    qconfig_dict: Any,
+    quantization_config: Union[PrepareQuantizationConfig, Dict[str, Any]],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
-    equalization_qconfig_dict: Optional[Dict[str, Any]] = None,
+    equalization_config: Optional[Union[EqualizationConfig, Dict[str, Any]]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
 ) -> ObservedGraphModule:
+    # TODO(andrew): update this comment
     r""" Prepare a model for post training static quantization
 
     Args:
@@ -488,17 +506,17 @@ def prepare_fx(
     torch._C._log_api_usage_once("quantization_api.quantize_fx.prepare_fx")
     return _prepare_fx(
         model,
-        qconfig_dict,
+        quantization_config,
         False,  # is_qat
         prepare_custom_config_dict,
-        equalization_qconfig_dict,
+        equalization_config,
         backend_config_dict,
     )
 
 
 def prepare_qat_fx(
     model: torch.nn.Module,
-    qconfig_dict: Any,
+    quantization_config: Union[PrepareQuantizationConfig, Dict[str, Any]],
     prepare_custom_config_dict: Optional[Dict[str, Any]] = None,
     backend_config_dict: Optional[Dict[str, Any]] = None,
 ) -> ObservedGraphModule:
@@ -506,12 +524,12 @@ def prepare_qat_fx(
 
     Args:
       * `model`: torch.nn.Module model, must be in train mode
-      * `qconfig_dict`: see :func:`~torch.ao.quantization.prepare_fx`
+      * `quantization_config`: see :func:`~torch.ao.quantization.prepare_fx`
       * `prepare_custom_config_dict`: see :func:`~torch.ao.quantization.prepare_fx`
       * `backend_config_dict`: see :func:`~torch.ao.quantization.prepare_fx`
 
     Return:
-      A GraphModule with fake quant modules (configured by qconfig_dict), ready for
+      A GraphModule with fake quant modules (configured by quantization_config), ready for
       quantization aware training
 
     Example::
@@ -536,7 +554,7 @@ def prepare_qat_fx(
     torch._C._log_api_usage_once("quantization_api.quantize_fx.prepare_qat_fx")
     return _prepare_fx(
         model,
-        qconfig_dict,
+        quantization_config,
         True,  # is_qat
         prepare_custom_config_dict,
         backend_config_dict=backend_config_dict,
@@ -549,13 +567,21 @@ def _convert_fx(
     convert_custom_config_dict: Optional[Dict[str, Any]] = None,
     is_standalone_module: bool = False,
     _remove_qconfig: bool = True,
-    qconfig_dict: Dict[str, Any] = None,
+    quantization_config: Union[ConvertQuantizationConfig, Dict[str, Any]] = None,
     backend_config_dict: Dict[str, Any] = None,
 ) -> torch.nn.Module:
     """ `is_standalone_module`: see docs in :func:`~torch.ao.quantization.prepare_standalone_module_fx`
     """
     if convert_custom_config_dict is None:
         convert_custom_config_dict = {}
+
+    if isinstance(quantization_config, Dict):
+        warnings.warn(
+            "Passing a QConfig dictionary to convert is deprecated and will not be supported "
+            "in a future version. Please pass in a ConvertQuantizationConfig instead.")
+        quantization_config = ConvertQuantizationConfig.from_dict(quantization_config)
+    quantization_config = copy.deepcopy(quantization_config)
+    assert(quantization_config is None or isinstance(quantization_config, ConvertQuantizationConfig))
 
     _check_is_graph_module(graph_module)
     check_is_valid_convert_custom_config_dict(convert_custom_config_dict)
@@ -566,7 +592,7 @@ def _convert_fx(
         convert_custom_config_dict,
         is_standalone_module,
         _remove_qconfig_flag=_remove_qconfig,
-        convert_qconfig_dict=qconfig_dict,
+        quantization_config=quantization_config,
         backend_config_dict=backend_config_dict,
     )
 
@@ -581,9 +607,10 @@ def convert_fx(
     is_reference: bool = False,
     convert_custom_config_dict: Optional[Dict[str, Any]] = None,
     _remove_qconfig: bool = True,
-    qconfig_dict: Dict[str, Any] = None,
+    quantization_config: Union[ConvertQuantizationConfig, Dict[str, Any]] = None,
     backend_config_dict: Dict[str, Any] = None,
 ) -> torch.nn.Module:
+    # TODO(andrew): edit this comment
     r""" Convert a calibrated or trained model to a quantized model
 
     Args:
@@ -660,7 +687,7 @@ def convert_fx(
         is_reference,
         convert_custom_config_dict,
         _remove_qconfig=_remove_qconfig,
-        qconfig_dict=qconfig_dict,
+        quantization_config=quantization_config,
         backend_config_dict=backend_config_dict,
     )
 
